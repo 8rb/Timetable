859a8cb0152363b9697696cfc3835e4466c7024c
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-03-18 22:29:03 -0500

    ✨onClick Event
    Added Course info onClick and changed data cell rendered.

--- src/components/Timetable.tsx
+++ src/components/Timetable.tsx
@@ -1,11 +1,45 @@
 import React, { useState } from 'react'
-import { TableContainer, Table, TH, TD, TDTime } from './TimetableStyles';
+import { TableContainer, Table, TH, TD, TDTime, Modal, ModalSubTitleRow, ModalTitle, ModalSubTitle, ModalText } from './TimetableStyles';
 import TimeTableData from '../events';
+
+type Class = {
+    name: string,
+    color: string,
+    code: string,
+    class: string,
+    professor: string,
+    type?: string
+}
 
 const Timetable = () => {
 
     const [titles] = useState(['Time', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
     const [data] = useState(TimeTableData);
+    const [showModal, setShowModal] = useState(false);
+    const [actualClass, setActualClass] = useState<Class>();
+
+
+    const renderModal = () => {
+        return actualClass && 
+        <Modal color={actualClass.color} onClick={() => {toggleModal(undefined)}}>
+            <ModalTitle>{actualClass.name}</ModalTitle>
+            <ModalSubTitleRow>
+                <ModalSubTitle>{actualClass.code}</ModalSubTitle>
+                <ModalSubTitle>{actualClass.class}</ModalSubTitle>
+            </ModalSubTitleRow>
+            <ModalText>{actualClass.professor}</ModalText>
+        </Modal>
+    }
+
+    const toggleModal = (clickedClass: Class | undefined) => {
+        if(clickedClass) {
+            setShowModal(true);
+            setActualClass(clickedClass);
+        }
+        else {
+            setShowModal(false);
+        }
+    }
 
     const renderTableHeader = () => {
         return titles.map((key, index) => {
@@ -13,30 +47,48 @@ const Timetable = () => {
         })
     }
 
+    const renderDataCell = (actualClass: Class | undefined, type: string | undefined) => {
+        if(type === "first") {
+            return <TD onClick={() => {toggleModal(actualClass)}} color={actualClass?.color} type={type}>{actualClass?.name}</TD>
+        }
+        else if(type === "last") {
+            return <TD onClick={() => {toggleModal(actualClass)}} color={actualClass?.color} type={type}>{actualClass?.class}</TD>
+        }
+        else if(type === "middle") {
+            return <TD onClick={() => {toggleModal(actualClass)}} color={actualClass?.color} type={type}>{'-'}</TD>
+        }
+        else {
+            return <TD onClick={() => {toggleModal(undefined)}}></TD>
+        }
+    }
+
     const renderData = () => {
         return data.map(({time, monday, tuesday, wednesday, friday, saturday}, index) => {
             return <tr key={index}>
                 <TDTime key={index}>{time}</TDTime>
-                <TD color={monday?.class.color} type={monday?.type}>{monday?.class.name}</TD>
-                <TD color={tuesday?.class.color} type={tuesday?.type}>{tuesday?.class.name}</TD>
-                <TD color={wednesday?.class.color} type={wednesday?.type}>{wednesday?.class.name}</TD>
-                <TD></TD>
-                <TD color={friday?.class.color} type={friday?.type}>{friday?.class.name}</TD>
-                <TD color={saturday?.class.color} type={saturday?.type}>{saturday?.class.name}</TD>
+                {renderDataCell(monday?.class, monday?.type)}
+                {renderDataCell(tuesday?.class, tuesday?.type)}
+                {renderDataCell(wednesday?.class, wednesday?.type)}
+                {renderDataCell(undefined, undefined)} {/* Thursday */} 
+                {renderDataCell(friday?.class, friday?.type)}
+                {renderDataCell(saturday?.class, saturday?.type)}
             </tr>
         })
     }
     return (
-        <TableContainer>
-            <Table>
-                <thead>
-                    <tr>{renderTableHeader()}</tr>
-                </thead>
-                <tbody>
-                    {renderData()}
-                </tbody>
-            </Table>
-        </TableContainer>
+        <>
+            <TableContainer>
+                <Table>
+                    <thead>
+                        <tr>{renderTableHeader()}</tr>
+                    </thead>
+                    <tbody>
+                        {renderData()}
+                    </tbody>
+                </Table>
+            </TableContainer>
+            {showModal && renderModal()}
+        </>
     )
 }
 
