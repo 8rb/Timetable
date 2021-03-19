00d6100a66d8865988051fa0c868574905ea6130
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-03-18 20:35:11 -0500

    🎨 Dynamic Border Radius
    Added border radius based on time of class using styled-components.

--- src/events.ts
+++ src/events.ts
@@ -1,72 +1,83 @@
 type Class = {
     name: string,
     color: string,
-    code: string
+    code: string,
+    class: string,
+    professor: string,
+    type?: string
 }
 
 const Mobile: Class = {
-    "name": "Mobile",
-    "color": "#7DFFD8",
-    "code": "WX71"
+    name: "Mobile",
+    color: "#7DFFD8",
+    code: "CC183",
+    class: "WX71",
+    professor: "David Gerardo Quevedo Velasco"
 };
 
 const Artificial: Class = {
-    "name": "AI",
-    "color": "#FFDD7D",
-    "code": "WV71"
+    name: "AI",
+    color: "#FFDD7D",
+    code: "SI404",
+    class: "WV71",
+    professor: "Hugo David Calderon Vilca"
 };
 
 const Fas: Class = {
-    "name": "FAS",
-    "color": "#CF7DFF",
-    "code": "WX71"
+    name: "FAS",
+    color: "#CF7DFF",
+    code: "SI657",
+    class: "WX71",
+    professor: "Juan Antonio Flores Moroco"
 };
 
 const Concurrent: Class = {
-    "name": "Concurrent",
-    "color": "#7DBAFF",
-    "code": "SW72"
+    name: "Concurrent",
+    color: "#7DBAFF",
+    code: "CC65",
+    class: "SW72",
+    professor: "Carlos Alberto Jara Garcia"
 };
 
 const French: Class = {
-    "name": "French",
-    "color": "#FF7D7D",
-    "code": "TR66"
+    name: "French",
+    color: "#FF7D7D",
+    code: "TR224",
+    class: "TR66",
+    professor: "Paolo Rodrigo Nery Lozada"
 };
 
 const TimeTableData = [
     {
         "time": "7am",
-        "monday": Mobile,
-        "wednesday": { "color": "", "name": ""},
-        "thursday": { "color": "", "name": ""},
-        "saturday": { "color": "", "name": ""},
+        "monday": {class: Mobile, type: "first"},
+        "thursday": {},
     },
     {
         "time": "8am",
-        "monday": Mobile,
+        "monday": {class: Mobile, type: "last"},
     },
     {
         "time": "9am",
-        "tuesday": Artificial
+        "tuesday": {class: Artificial, type: "first"},
     },
     {
         "time": "10am",
-        "monday": Artificial,
-        "tuesday": Artificial,
-        "saturday": Fas
+        "monday": {class: Artificial, type: "first"},
+        "tuesday": {class: Artificial, type: "last"},
+        "saturday": {class: Fas, type: "first"},
     },
     {
         "time": "11am",
-        "monday": Artificial,
-        "friday": Mobile,
-        "saturday": Fas
+        "monday": {class: Artificial, type: "middle"},
+        "friday": {class: Mobile, type: "first"},
+        "saturday": {class: Fas, type: "middle"}
     },
     {
         "time": "12pm",
-        "monday": Artificial,
-        "friday": Mobile,
-        "saturday": Fas
+        "monday": {class: Artificial, type: "last"},
+        "friday": {class: Mobile, type: "last"},
+        "saturday": {class: Fas, type: "last"}
     },
     {
         "time": "1pm",
@@ -76,27 +87,27 @@ const TimeTableData = [
     },
     {
         "time": "5pm",
-        "tuesday": French,
-        "wednesday": French,
+        "tuesday": {class: French, type: "first"},
+        "wednesday": {class: French, type: "first"}
     },
     {
         "time": "6pm",
-        "tuesday": French,
-        "wednesday": French,
+        "tuesday": {class: French, type: "last"},
+        "wednesday": {class: French, type: "last"},
     },
     {
         "time": "7pm",
-        "monday": Fas,
-        "friday": Concurrent,
+        "monday": {class: Fas, type: "first"},
+        "friday": {class: Concurrent, type: "first"},
     },
     {
         "time": "8pm",
-        "monday": Fas,
-        "friday": Concurrent,
+        "monday": {class: Fas, type: "last"},
+        "friday": {class: Concurrent, type: "middle"},
     },
     {
         "time": "9pm",
-        "friday": Concurrent,
+        "friday": {class: Concurrent, type: "last"},
     }
 ]
 export default TimeTableData;
